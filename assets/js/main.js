be925bf0702cb838ecc80413461f0f1ee128700a
gluke0 <milanesigianluca@gmail.com>
2023-03-24 16:16:07 +0100

    console numeri + testo

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,21 @@ stampare in console i numeri da 1 a 100
         if (n % 3 == 0)
     numeri multipli di 5 -> Buzz
         if (n % 5 == 0)
+    numeri multipli di 3 e 5 -> FizzBuzz
+        if (n % 3 == 0 && n % 5 == 0)
 */
 
 
 for (let i = 1; i <= 100; i++){
-    console.log(i)
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log("FizzBuzz")
+    } else if (i % 5 == 0){
+        console.log("Buzz")
+    } else if (i % 3 == 0){
+        console.log("Fizz")
+    } else {
+        console.log(i)
+    }
 }
 
 
