9134ced9dc9c08a4b5cd145b8d7e2b3bc01a6ede
gluke0 <milanesigianluca@gmail.com>
2023-03-24 15:56:04 +0100

    console 1-100 normale

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+/* 
+stampare in console i numeri da 1 a 100
+    numeri multipli di 3 -> Fizz
+        if (n % 3 == 0)
+    numeri multipli di 5 -> Buzz
+        if (n % 5 == 0)
+*/
+
+
+for (let i = 1; i <= 100; i++){
+    console.log(i)
+}
+
+
